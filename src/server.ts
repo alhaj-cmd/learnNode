4d7286fc71def5ff959725c2fc99cddc68407ea0
alhaj-cmd <alhazsarkar5523@gmail.com>
2026-05-10 21:13:41 +0600

    config to the folder & file

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import {createServer, IncomingMessage, Server} from "http";
+const server: Server = createServer((req : IncomingMessage, res)=>{
+    console.log(req)
+})
+
+server.listen(5000,()=>{
+    console.log("server is running on the port")
+})
