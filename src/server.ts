d798db2b9510ce57f5e3bb964f2273273c468266
alhaj-cmd <alhazsarkar5523@gmail.com>
2026-05-11 08:59:42 +0600

    server site added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,23 @@
-import {createServer, IncomingMessage, Server} from "http";
-const server: Server = createServer((req : IncomingMessage, res)=>{
-    console.log(req)
+import {createServer, IncomingMessage, Server, ServerResponse} from "http";
+import { url } from "inspector";
+const server: Server = createServer((req : IncomingMessage, res: ServerResponse)=>{
+    // console.log(req.url);
+    // console.log(req.method)
+    const url = req.url;
+    const method = req.method;
+    if(url === "/" && method=== 'GET'){
+        // console.log("This is root route")
+        res.writeHead(200, {"content-type": "application/json"});
+        res.end(JSON.stringify({message:"This is root route"}))
+    } else if(url?.startsWith('/products')){
+        res.writeHead(200, {"content-type": "application/json"});
+        res.end(JSON.stringify({message:"This is root product site"}))
+    }
+     else{
+        res.writeHead(404, {'content-type': 'application/json'});
+        res.end(JSON.stringify({message:"Route not found"}));
+    }
+
 })
 
 server.listen(5000,()=>{
