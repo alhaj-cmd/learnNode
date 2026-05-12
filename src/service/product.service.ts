175edcf19611130532194ab5981a1da92228423b
alhaj-cmd <alhazsarkar5523@gmail.com>
2026-05-12 10:38:20 +0600

    path file added

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,4 +1,6 @@
+import path from "node:path";
 
+const filePath = path.join(process.cwd(), "./src/database/db.json");
 export const readProduct = ()=>{
-    console.log(process.cwd());
+    console.log('Data pawa gece',filePath);
 }
