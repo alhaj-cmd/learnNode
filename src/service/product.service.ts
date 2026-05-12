94de912595c527940322d2cb450fd296fe875a77
alhaj-cmd <alhazsarkar5523@gmail.com>
2026-05-12 10:46:13 +0600

    filepath consoling

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,6 +1,9 @@
+import fs from "fs";
 import path from "node:path";
 
 const filePath = path.join(process.cwd(), "./src/database/db.json");
 export const readProduct = ()=>{
-    console.log('Data pawa gece',filePath);
+    // console.log('Data pawa gece',filePath);
+    const products = fs.readFileSync(filePath);
+    console.log(products);
 }
